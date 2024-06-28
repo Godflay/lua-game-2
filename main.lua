cf351585aafe93c7d05c89e74f9c3df445649802
Godflay <gg.godfreygabriel@gmail.com>
2024-06-28 11:09:31 -0400

    create assets and player table and drawing on the screen

---
+++ main.lua
@@ -0,0 +1,24 @@
+function love.load()
+    --assets table
+    sprites = {}
+    sprites.background = love.graphics.newImage('assets/background.png')
+    sprites.bullet = love.graphics.newImage('assets/bullet.png')
+    sprites.player = love.graphics.newImage('assets/player.png')
+    sprites.zombie = love.graphics.newImage('assets/zombie.png')
+
+    --player table
+    player = {}
+    --player position middle of the screen
+    player.x = love.graphics.getWidth() / 2
+    player.y = love.graphics.getHeight() / 2
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.draw(sprites.background, 0, 0)
+
+    love.graphics.draw(sprites.player, player.x, player.y)
+end
